c0cbc5afc6d0fd6f7aa25373e596fde44c9e8d6c
MiloMontuori <milo.montuori@student.junia.com>
2022-09-12 09:04:01 +0200

    Initial commit

---
+++ build/CMakeFiles/TP01.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP01.dir/first.c.obj"
+  "CMakeFiles/TP01.dir/first.c.obj.d"
+  "TP01.exe"
+  "TP01.exe.manifest"
+  "TP01.pdb"
+  "libTP01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TP01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Administrateur/Desktop/22-23
+# Build directory: C:/Users/Administrateur/Desktop/22-23/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
